5ec5c6923e8643a56d762171d396127d66c0e1a9
arman-ali-786 <147016079+arman-ali-786@users.noreply.github.com>
2023-10-05 09:07:51 +0530

    Add files via upload

---
+++ election.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class election{
+public static void main(String args[]){
+Scanner sc = new Scanner(System.in);
+int n = sc.nextInt();
+if(n>=18)
+{
+System.out.println("you are eligible to vote.");
+}
+else{
+System.out.println("you are not eligible to vote. ")
+}
+}
+}
