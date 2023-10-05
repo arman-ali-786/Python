abfe7619a0cce7d6aa6b37b6ea289eca6e5141cb
arman-ali-786 <147016079+arman-ali-786@users.noreply.github.com>
2023-10-05 08:56:21 +0530

    Update Python.py

--- Python.py
+++ Python.py
@@ -1,7 +1,7 @@
 ch= input("Enter any character ")
-if(ch>='A' and ch='Z'):
+if(ch>='A' and ch<='Z'):
   print (ch," is an uppercase ")
-elif(ch>='a' and ch='z')
+elif(ch>='a' and ch<='z')
   print(ch," is a lowercase ")
 else:
   print(" Character entered is neither uppercase nor lowercase ")
